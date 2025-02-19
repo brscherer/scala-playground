cae2341fd115da79e0e422ab7271b3ca6162de5e
Bruno Scherer <bruno.raphael96@gmail.com>
2025-02-18 21:14:11 -0300

    add dynamic programming example fibonacci

---
+++ scala-dynamic-programming/src/main/scala/Fibonacci.scala
@@ -0,0 +1,11 @@
+import scala.collection.mutable
+
+// F(n) = F(n-1) + F(n-2).
+def fib(n: Int, memo: mutable.Map[Int, Int] = mutable.Map()): Int = {
+  if (n <= 1) return n
+  memo.getOrElseUpdate(n, fib(n - 1, memo) + fib(n - 2, memo))
+}
+
+@main
+def main(): Unit =
+  println(fib(10)) // Output: 55
